62ead5c89c6d1e868199642d4a1ccea5e24d064d
Paul Morales <paulmorales@pauls-mbp.mynetworksettings.com>
2025-03-18 18:59:25 -0400

    completed form structure, working on functionality

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,78 +1,134 @@
+import React, { useRef } from "react";
 import bgImg from '../assets/VeteransPic.jpg';
 import { BsList } from "react-icons/bs";
 import { GrLock } from "react-icons/gr";
 import { FaRegUser } from "react-icons/fa6";
 import { GrMailOption } from "react-icons/gr";
-import './landingPage.css';
+import './LandingPage.css';
 
-function landingPage() {
+function LandingPage() {
+
+    const registerRef = useRef(null);
+    const loginRef = useRef(null);
+
+
+    function register() {
+        if (registerRef.current) {
+            loginRef.current.style.right = "-510px";
+            registerRef.current.style.left = "5px";
+          }
+        console.log("pressed")
+    }
+
+    function login() {
+        if (loginRef.current) {
+            loginRef.current.style.left = "4px"; // Move the login form to the left by 4px
+        }
+        if (registerRef.current) {
+            registerRef.current.style.right = "-520px"; // Move the register form out of view
+        }
+        console.log("pressed")
+    }
     return (
-        <div class="wrapper">
-            <nav class="nav">
-                <div class="nav-logo">
+        <div className="wrapper">
+            <nav className="nav">
+                <div className="nav-logo">
                     <p>Patriot Path</p>
                 </div>
-                <div class="nav-menu">
+                <div className="nav-menu">
                     <ul>
-                        <li><a href="#" class="link active">Home</a></li>
-                        <li><a href="#" class="link">Blog</a></li>
-                        <li><a href="#" class="link">Services</a></li>
-                        <li><a href="#" class="link">About</a></li>
+                        <li><a href="#" className="link active">Home</a></li>
+                        <li><a href="#" className="link">Blog</a></li>
+                        <li><a href="#" className="link">Services</a></li>
+                        <li><a href="#" className="link">About</a></li>
 
                     </ul>
                 </div>
-                <div class="nav-button">
-                    <button class="btn white-btn" id="loginBtn">Sign In</button>
-                    <button class="btn" id="registerBtn">Sign Up</button>
+                <div className="nav-button">
+                    <button className="btn white-btn" id="loginBtn" onClick={login}>Sign In</button>
+                    <button className="btn" id="registerBtn" onClick={register}> Sign Up</button>
                 </div>
-                <div class="nav-menu-btn">
+                <div className="nav-menu-btn">
                     <BsList />
                 </div>
 
             </nav>
 
                     {/* Form Box */}
+            <div className="form-box">
 
-            <div class="form-box">
+                    {/* Login form */}
+                <div className="login-container" ref={loginRef} id="login">
+                    <div className="top">
+                        <span onClick={login}>Don't have an Account? <a href="#" >Sign Up</a></span>
+                        <header>Login</header>
+                    </div>
+
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="Username or Email"/>
+                            <FaRegUser className="icon"/>
+                        </div>
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="Password"/>
+                            <GrLock className="icon"/>
+                        </div>
+                        <div className="input-box">
+                            <input type="submit" className="submit" value="Register"/>
+                        </div>
+
+                        <div className="two-col"> 
+                            <div className="one">
+                                <input type="checkbox" id="login-check"/>
+                                <label htmlFor="login-check">Remember Me</label>
+                            </div>
+                            <div className="two">
+                                <label><a href="#">Forgot Password?</a></label>
+                            </div>
+
+                        </div>
+                    
+                </div>
+
                     {/* Registration form */}
-                <div class="register-container" id="register">
-                    <div class="top">
-                        <span>Have an Account? <a href="#" onClick="login()">Login</a></span>
+                <div className="register-container" ref={registerRef} id="register">
+                    <div className="top">
+                        <span onClick={register}>Have an Account? <a href="#" >Sign In</a></span>
                         <header>Sign Up</header>
                     </div>
 
-                    <div class="two-forms">
-                        <div class="input-box">
-                            <input type="text" class="input-field" placeholder="First Name"/>
-                            <FaRegUser />
+                    <div className="two-forms">
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="First Name"/>
+                            <FaRegUser className="icon"/>
                         </div>
-                        <div class="input-box">
-                            <input type="text" class="input-field" placeholder="Last Name"/>
-                            <FaRegUser />
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="Last Name"/>
+                            <FaRegUser className="icon"/>
                         </div>
-                        <div class="input-box">
-                            <input type="text" class="input-field" placeholder="Email"/>
-                            <GrMailOption />
+                    </div>
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="Email"/>
+                            <GrMailOption className="icon"/>
                         </div>
-                        <div class="input-box">
-                            <input type="text" class="input-field" placeholder="Password"/>
-                            <GrLock />
+                        <div className="input-box">
+                            <input type="text" className="input-field" placeholder="Password"/>
+                            <GrLock className="icon"/>
                         </div>
-                        <div class="input-box">
-                            <input type="submit" class="submit" value="Register"/>
+                        <div className="input-box">
+                            <input type="submit" className="submit" value="Register"/>
                         </div>
 
-                        <div class="two-col"> 
-                            <div class="one">
+                        <div className="two-col"> 
+                            <div className="one">
                                 <input type="checkbox" id="register-check"/>
-                                <label for="register-check">Remember Me</label>
+                                <label htmlFor="register-check">Remember Me</label>
                             </div>
-                            <div class="two">
+                            <div className="two">
                                 <label><a href="#">Terms & Conditions</a></label>
                             </div>
 
                         </div>
-                    </div>
+                    
                 </div>
 
             </div>
@@ -81,4 +137,4 @@ function landingPage() {
     );
 }
 
-export default landingPage;
+export default LandingPage;
