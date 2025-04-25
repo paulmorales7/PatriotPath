c1d9ae50590d0b62089726288e509ac651cdf02a
Paul Morales <paulmorales@pauls-mbp.mynetworksettings.com>
2025-04-25 11:24:55 -0400

    starting on home page, first link to go on the nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Authentication from './components/authPage';
+import Home from './components/Home';
 
 function App() {
   return (
       <Router>
           <div className="App">
               <Authentication />
+              <Home />
           </div>
       </Router>
   );
